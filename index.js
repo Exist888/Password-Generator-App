cd469865402ac89dc451445b2b7670693a47cae3
Filip Herbst <filip_herbst@yahoo.com>
2024-12-28 17:10:29 -0600

    Coded more features
    Added dropdown for user to select password length. Added copy to clipboard feature for easy copy and pasting online.

--- index.js
+++ index.js
@@ -4,13 +4,47 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 let firstPassword = document.getElementById("first-password");
 let secondPassword = document.getElementById("second-password");
 
+function getPasswordLength() {
+    let passwordLength = document.getElementById("length-dropdown");
+    let passwordLengthAsNumber = Number(passwordLength.value);
+    return passwordLengthAsNumber;
+}
+
 function generatePassword() {
-    firstPassword.textContent = " ";
-    secondPassword.textContent = " ";
-    for (let passwordCharacter = 0; passwordCharacter < 15; passwordCharacter++) {
+    firstPassword.textContent = "";
+    secondPassword.textContent = "";
+    for (let passwordCharacter = 0; passwordCharacter < getPasswordLength(); passwordCharacter++) {
         let i = Math.floor( Math.random() * characters.length);
         let i2 = Math.floor( Math.random() * characters.length);
         firstPassword.textContent += characters[i];
         secondPassword.textContent += characters[i2];
+    }
+}
+
+function copyOnClick1() {
+    if (firstPassword.textContent === "" || firstPassword.textContent === null) {
+        alert("Please generate a password first. 😊");
+    } else {
+        let copyText1 = document.getElementById("first-password").innerText;
+        console.log(copyText1);
+        navigator.clipboard.writeText(copyText1).then(() => {
+            alert("Password successfully copied!");
+        }).catch(err => {
+            alert("Sorry, there seems to be a glitch. Please copy and paste your password manually.");
+        });
+    }
+}
+
+function copyOnClick2() {
+    if (secondPassword.textContent === "" || secondPassword.textContent === null) {
+        alert("Please generate a password first. 😊");
+    } else {
+        let copyText2 = document.getElementById("second-password").innerText;
+        console.log(copyText2);
+        navigator.clipboard.writeText(copyText2).then(() => {
+            alert("Password successfully copied!");
+        }).catch(err => {
+            alert("Sorry, there seems to be a glitch. Please copy and paste your password manually.");
+        });
     }
 }
